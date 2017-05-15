37e6abcc0ab04e07e7045dc4f7764b0d6a56f641
Stalin Thangapalam <stalinthangapalam@Stalins-MacBook.local>
2017-05-15 01:41:44 -0400

    :Added new structure

--- eNas-cf-service-broker/src/main/java/com/emc/eNas/cloudfoundry/broker/config/eNasBrokerConfigConstants.java
+++
@@ -1,8 +0,0 @@
-package com.emc.eNas.cloudfoundry.broker.config;
-
-public class eNasBrokerConfigConstants {
-	
-	public static final Object SERVICE_TYPE = "FileShare";
-	public static final Object FILE_SHARE = "FILE_SHARE";
-
-}
